c761c4956ebbcce65112fadb2f24e6be8527d1b3
Ed <ed.shaziman@cymantix.com>
2025-02-05 10:44:05 -0500

    cors middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi import FastAPI, Request
 from src.model.models import User
+from starlette.middleware.cors import CORSMiddleware
 from src.schemas.user import UserResponse, UserCreate
 from src.middleware.auth import AuthMiddleware
 from src.routers import health_check
@@ -18,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 
-
+# Add routers
 app.include_router(health_check.router, prefix="/health", tags=["health"])
 app.include_router(subscription_router, prefix="/subscription", tags=["subscription"])
 app.include_router(user_router, prefix="/user", tags=["user"])
@@ -27,10 +28,22 @@ app.include_router(expense_router, prefix="/expense", tags=["expense"])
 app.include_router(scope_router, prefix="/scope", tags=["scope"])
 app.include_router(project_router, prefix="/project", tags=["project"])
 
+# Add middleware
 app.add_middleware(AuthMiddleware)
 
 
 
+# Add cores
+
+origins = ["https://localhost:3000", "http://localhost:3000","https://localhost:3001", "http://localhost:3001"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/users", response_model=List[UserResponse])
 def get_tests(db: session):
