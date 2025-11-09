ee95ab8e266d3bf4e9c7fc09e262d9961a7b471c
Ed <ed.shaziman@cymantix.com>
2025-11-09 10:01:35 -0500

    update the redeme

--- src/services/bank_importers/chase_credit.py
+++ src/services/bank_importers/chase_credit.py
@@ -2,6 +2,7 @@ import csv
 import io
 from model.models import ImportJob
 from src.services.bank_importers.base import BaseBankImporter
+from util.transaction import clean_description, generate_fingerprint, get_amount_cents, get_date_from_row, get_internal_type
 
 
 class ChaseCreditImporter(BaseBankImporter):
@@ -12,3 +13,13 @@ class ChaseCreditImporter(BaseBankImporter):
         csv_reader = csv.DictReader(io.StringIO(self.file_content))
         transactions_and_fps = []
         fingerprints = []
+        for row in csv_reader:
+            type_ = row.get("Type")
+            internal_type = get_internal_type(type_, row.get("Description"))
+            amount_str = row.get("Amount", "0").strip()
+            amount_cents = get_amount_cents(amount_str)
+            date = get_date_from_row(row)
+            title = clean_description(row.get("Description"))
+            fingerprint = generate_fingerprint(
+                date=date, title=title, amount_cents=amount_cents
+            )            
